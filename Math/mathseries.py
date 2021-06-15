a55d6eb910ab3c7fec952c99e15f5dfee35f2701
datta-agni <dattadunga@gmail.com>
2021-06-16 00:14:12 +0530

    Removed.

--- Math/mathseries.py
+++
@@ -1,7 +0,0 @@
-# Sum of Series: 1 + x^2/2 + x^3/3 + … x^n/n.
-n = int(input("Enter the number of terms:"))
-x = int(input("Enter the value of x:"))
-sum1 = 1
-for i in range(2, n + 1):
-    sum1 = sum1 + ((x ** i) / i)
-print("The sum of series is", round(sum1, 2))
